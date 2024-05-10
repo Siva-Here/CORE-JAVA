971518e4b922bc8266953895411ae5d7847334c2
siva <sivahere9484@gmail.com>
2024-05-10 22:09:27 +0530

    prctice1

---
+++ Multiple.java
@@ -0,0 +1,25 @@
+public class Multiple {
+    public static void main(String[] args){
+
+    }
+}
+
+class A{
+    public void getA(){
+        System.out.println("From class A");
+    }
+}
+
+class B{
+    public void getB(){
+        System.out.println("From class B");
+    }
+}
+
+// class C extends A,B{
+//     public void getC(){
+//         System.out.println("From class C");
+//     }
+// }
+// //-------------------------->MULTIPLE INHERITANCE DOESNT SUPPORT IN JAVA THROUGH EXTENDS
+
